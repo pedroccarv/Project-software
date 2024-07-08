f4a51ab9654b450fa9bfbaa6186ef9770b9375b2
pedroccarv <ppedrinhohenrique06@hotmail.com>
2024-07-08 15:34:46 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
- import Cadastro from './cadastro'
- import AppRoutes from './route/route.jsx'
-
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppRoutes from './route/route';
+import './index.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="266963174901-1ro8upe7tgfc77pbba8pktf4qoorajit.apps.googleusercontent.com">
-    <AppRoutes />
-      </GoogleOAuthProvider>;
+      <AppRoutes />
+    </GoogleOAuthProvider>
   </React.StrictMode>,
-)
+);
