08a712ba5ca03e89312b1ff27a23205cda590eb1
pedroccarv <ppedrinhohenrique06@hotmail.com>
2024-07-08 15:34:39 -0300

    Create DetalhesPartida.jsx

---
+++ src/pages/DetalhesPartida.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Mapa from '../components/Mapa';
+
+function DetalhesPartida() {
+  const { id } = useParams();
+  const [partida, setPartida] = useState(null);
+
+  useEffect(() => {
+    axios.get(`/api/partidas/${id}`)
+      .then(response => {
+        setPartida(response.data);
+      })
+      .catch(error => {
+        console.error("Houve um erro ao buscar os detalhes da partida:", error);
+      });
+  }, [id]);
+
+  if (!partida) return <div>Carregando...</div>;
+
+  return (
+    <div>
+            <div className="rotas">
+      <Link to="/login" className="link-rota">Login</Link>
+      <Link to="/cadastro" className="link-rota">Cadastro</Link>
+      <Link to="/editar-perfil" className="link-rota">Editar Perfil</Link>
+      <Link to="/mapa" className="link-rota">Mapa</Link>
+      <Link to="/favoritar-partida" className="link-rota">Partidas Favoritas</Link>
+      <Link to="/detalhes-partida" className="link-rota">Detalhes Partidas</Link>
+      <Link to="/cadastro-partida" className="link-rota">Cadastro Partidas</Link>
+      <Link to="/historico-partidas" className="link-rota">Historico Partidas</Link>
+      <Link to="/pagamento" className="link-rota">Pagamento</Link>
+      <Link to="/contato" className="link-rota">Contato</Link></div>
+      <div className="container">
+      <h1>{partida.nome}</h1>
+      <p>{partida.data}</p>
+      <p>{partida.local}</p>
+      <p>{partida.tipoEsporte}</p>
+      <Mapa latitude={partida.latitude} longitude={partida.longitude} />
+    </div>
+    </div>
+
+  );
+}
+
+export default DetalhesPartida;
