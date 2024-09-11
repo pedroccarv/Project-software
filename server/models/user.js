0ae35563a9aa7179c9089cee2fe6721aa4a9f6c9
pedroccarv <ppedrinhohenrique06@hotmail.com>
2024-09-11 18:42:27 -0300

    Create user.js
    Configuracao de usuario

---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+// models/User.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.ENUM('user', 'admin'),
+    defaultValue: 'user',
+  }
+}, {
+  timestamps: true,
+});
+
+module.exports = User;
