d016614a87bbce9320d91ff64c9a711ef9bbe57f
guedes1710 <133006949+guedes1710@users.noreply.github.com>
2024-09-16 11:26:47 -0300

    Update route.jsx
    rota contador

--- src/route/route.jsx
+++ src/route/route.jsx
@@ -17,23 +17,35 @@ import UploadImagem from '../components/UploadImagem';
 import AvaliacaoPartida from '../components/AvaliacaoPartida';
 import ConvidarAmigos from '../components/ConvidarAmigos';
 import HistoricoConquistas from '../components/HistoricoConquistas';
-import Imagem from '../components/Imagem'
-import Agendar from '../components/Agendarquadra'
+import Agendar from '../components/Agendarquadra';
+import ContadorPartidas from '../components/ContadorPartidas'; 
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Página inicial de Partidas */}
         <Route path="/" element={<Partidas />} />
+
+        {/* Rotas de autenticação */}
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/recuperar-senha" element={<RecuperarSenha />} />
+
+        {/* Rotas de perfil e ajustes */}
         <Route path="/editar-perfil" element={<EditarPerfil />} />
+
+        {/* Mapa e funcionalidades de arena */}
         <Route path="/mapa" element={<Mapa />} />
         <Route path="/favoritar-partida" element={<FavoritarPartida />} />
+
+        {/* Rotas relacionadas a partidas */}
         <Route path="/detalhes-partida/:id" element={<DetalhesPartida />} />
         <Route path="/cadastro-partida" element={<CadastroPartida />} />
         <Route path="/historico-partidas" element={<HistoricoPartidas />} />
+        <Route path="/contador-partidas" element={<ContadorPartidas />} /> {/* Nova rota */}
+
+        {/* Funcionalidades adicionais */}
         <Route path="/pagamento" element={<Pagamento />} />
         <Route path="/contato" element={<Contato />} />
         <Route path="/chat" element={<Chat />} />
@@ -42,8 +54,9 @@ function AppRoutes() {
         <Route path="/avaliacao-partida" element={<AvaliacaoPartida />} />
         <Route path="/convidar-amigos" element={<ConvidarAmigos />} />
         <Route path="/historico-conquistas" element={<HistoricoConquistas />} />
-        <Route path="/imagem" element={<Imagem/>} />
-        <Route path="/agendar" element={<Agendar/>} />
+        
+        {/* Agendamento de quadra */}
+        <Route path="/agendar" element={<Agendar />} />
       </Routes>
     </BrowserRouter>
   );
