55afe40908bd7b9bef267eaeb32a003a743ad71d
guedes1710 <133006949+guedes1710@users.noreply.github.com>
2024-10-06 14:29:51 -0300

    Update DetalhesPartida.jsx
    detalhes da partida

--- src/pages/DetalhesPartida.jsx
+++ src/pages/DetalhesPartida.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Mapa from '../components/Mapa';
+import '../styles/DetalhesPartida.css';
 
 function DetalhesPartida() {
   const { id } = useParams();
