47c2181adc31a7d3291aa4f553a8172c1fb24868
pedroccarv <ppedrinhohenrique06@hotmail.com>
2024-10-11 15:15:26 -0300

    Update Cadastro.jsx
    adicionando use effect, adicionando o fontAwesome, funçoes de menu suspenso,

--- src/components/Cadastro.jsx
+++ src/components/Cadastro.jsx
@@ -1,7 +1,9 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/cadastro.css';
 import api from '../services/api';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 function Cadastro() {
   const [email, setEmail] = useState('');
@@ -9,11 +11,40 @@ function Cadastro() {
   const [password, setPassword] = useState('');
   const [nome, setNome] = useState('');
   const [age, setAge] = useState('');
+  const [menuAberto, setMenuAberto] = useState(false);
+  const [senhaVisivel, setSenhaVisivel] = useState(false);
 
+  const menuRef = useRef(null); // Para referenciar o menu
   const inputEmail = useRef()
   const inputAge = useRef()
   const inputNome = useRef()
   const inputPassword = useRef()
+
+  const toggleMenu = () => {
+    setMenuAberto(!menuAberto);
+  };
+
+  const closeMenu = () => {
+    setMenuAberto(false);
+  };
+
+  // Função para fechar o menu se clicar fora
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      closeMenu();
+    }
+  };
+
+  useEffect(() => {
+    // Adiciona e remove o evento de clique
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+  const toggleSenhaVisivel = () => {
+    setSenhaVisivel(!senhaVisivel);
+  };
 
   async function createUsers() {
     try {
@@ -37,31 +68,40 @@ function Cadastro() {
   };
 
   return (
-    <div>
-      <div className="rotas">
-        <Link to="/login" className="link-rota">Login</Link>
-        <Link to="/cadastro" className="link-rota">Cadastro</Link>
-        <Link to="/editar-perfil" className="link-rota">Editar Perfil</Link>
-        <Link to="/mapa" className="link-rota">Mapa</Link>
-        <Link to="/favoritar-partida" className="link-rota">Partidas Favoritas</Link>
-        <Link to="/detalhes-partida" className="link-rota">Detalhes Partidas</Link>
-        <Link to="/cadastro-partida" className="link-rota">Cadastro Partidas</Link>
-        <Link to="/historico-partidas" className="link-rota">Historico Partidas</Link>
-        <Link to="/pagamento" className="link-rota">Pagamento</Link>
-        <Link to="/contato" className="link-rota">Contato</Link>
-        <Link to="/historico-conquistas" className="link-rota">Historico  conquistas</Link>
-        <Link to="/notificacoes" className="link-rota">Notificacoes</Link>
-        <Link to="/chat" className="link-rota">Chat</Link>
-        <Link to="/upload-imagem" className="link-rota">Imagem</Link>
-        <Link to="/avaliacao-partida" className="link-rota">Avaliacao partida</Link>
-        <Link to="/convidar-amigos" className="link-rota">Convidar amigos</Link>
-      </div>
-      <img src="src/images/logo.png" alt="" width="60%" />
-      <span></span>
-      <div className="cadastro">
+    <div className="cadastro-page">
+      {/* Botão de Menu totalmente à esquerda */}
+      <button className="menu-button" onClick={toggleMenu}>
+        Menu
+      </button>
+
+      {/* Menu Lateral */}
+      {menuAberto && (
+        <div className="menu" ref={menuRef}>
+          <Link to="/login" onClick={closeMenu}>Login</Link>
+          <Link to="/cadastro" onClick={closeMenu}>Cadastro</Link>
+          <Link to="/editar-perfil" onClick={closeMenu}>Editar Perfil</Link>
+          <Link to="/mapa" onClick={closeMenu}>Mapa</Link>
+          <Link to="/favoritar-partida" onClick={closeMenu}>Partidas Favoritas</Link>
+          <Link to="/detalhes-partida" onClick={closeMenu}>Detalhes Partidas</Link>
+          <Link to="/cadastro-partida" onClick={closeMenu}>Cadastro Partidas</Link>
+          <Link to="/historico-partidas" onClick={closeMenu}>Histórico Partidas</Link>
+          <Link to="/pagamento" onClick={closeMenu}>Pagamento</Link>
+          <Link to="/contato" onClick={closeMenu}>Contato</Link>
+          <Link to="/historico-conquistas" onClick={closeMenu}>Histórico de Conquistas</Link>
+          <Link to="/notificacoes" onClick={closeMenu}>Notificações</Link>
+          <Link to="/chat" onClick={closeMenu}>Chat</Link>
+          <Link to="/upload-imagem" onClick={closeMenu}>Imagem</Link>
+          <Link to="/avaliacao-partida" onClick={closeMenu}>Avaliação Partida</Link>
+          <Link to="/convidar-amigos" onClick={closeMenu}>Convidar Amigos</Link>
+        </div>
+      )}
+
+      {/* Contêiner de Cadastro centralizado */}
+      <div className="cadastro-container">
+        <img src="src/images/logo.png" alt="Logo" width="60%" />
         <h1>Crie uma conta</h1>
         <form onSubmit={handleSubmit}>
-        <input
+          <input
             type="text"
             placeholder="Nome"
             value={nome}
@@ -77,30 +117,35 @@ function Cadastro() {
             ref={inputAge}
             required
           />
-          <input
-            type="email"
+          <input 
+            type="email" 
             placeholder="E-mail"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             ref={inputEmail}
-            required
+            required 
           />
-          <input
-            type="email"
-            placeholder="Verifique seu e-mail"
+          <input 
+            type="email" 
+            placeholder="Verifique seu e-mail" 
             value={verifyEmail}
             onChange={(e) => setVerifyEmail(e.target.value)}
             required
           />
-          <input
-            type="password"
-            placeholder="Senha"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            ref={inputPassword}
-            required
-          />
-          <button type="submit"  onClick={createUsers} className="btn-cadastrar">Criar perfil gratuito</button>
+          <div className="senha-container">
+            <input 
+              type={senhaVisivel ? "text" : "password"} 
+              placeholder="Senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              ref={inputPassword}
+              required
+            />
+            <span className="eye-icon" onClick={toggleSenhaVisivel}>
+              <FontAwesomeIcon icon={senhaVisivel ? faEye : faEyeSlash} />
+            </span>
+          </div>
+          <button type="submit" onClick={createUsers} className="btn-cadastrar">Criar perfil gratuito</button>
         </form>
         <p className="terms">
           Ao se registrar, você está de acordo com nossos <a href="#">termos e condições</a> e confirma estar ciente de nossa <a href="#">política de privacidade</a>.
@@ -109,7 +154,6 @@ function Cadastro() {
           Já tem uma conta? <Link to="/login">Faça seu login!</Link>
         </p>
       </div>
-
     </div>
   );
 }
