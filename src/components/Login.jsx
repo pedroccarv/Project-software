125de9cd36f6d7674b3176502387af39a778ac1e
pedroccarv <ppedrinhohenrique06@hotmail.com>
2024-10-22 17:23:23 -0300

    Update Login.jsx
    Redirecionamento

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom'; // Importar useNavigate
 import '../styles/Login.css';
 import api from '../services/api';
+import { useAuth } from '../services/AuthContext';
 
 function Login() {
+  const { login } = useAuth();
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   const [lembrar, setLembrar] = useState(false);
+  const navigate = useNavigate(); // Inicializar o hook useNavigate
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -28,8 +31,9 @@ function Login() {
         const response = await api.post('/login', { email, password: senha });
         if (response && response.data) {
           localStorage.setItem('userId', response.data.id);
+          login(response.data);
           alert('Login bem-sucedido!');
-          window.location.href = '/';
+          navigate('/'); // Usar navigate para redirecionar
         } else {
           throw new Error('Resposta inválida da API');
         }
@@ -40,56 +44,54 @@ function Login() {
       alert('Preencha os campos de email e senha');
     }
   };
-  
 
   return (
     <div>
-    <div className="rotas">
-    <Link to="/login" className="link-rota">Login</Link>
-    <Link to="/cadastro" className="link-rota">Cadastro</Link>
-    <Link to="/editar-perfil" className="link-rota">Editar Perfil</Link>
-    <Link to="/mapa" className="link-rota">Mapa</Link>
-    <Link to="/favoritar-partida" className="link-rota">Partidas Favoritas</Link>
-    <Link to="/detalhes-partida" className="link-rota">Detalhes Partidas</Link>
-    <Link to="/cadastro-partida" className="link-rota">Cadastro Partidas</Link>
-    <Link to="/historico-partidas" className="link-rota">Historico Partidas</Link>
-    <Link to="/pagamento" className="link-rota">Pagamento</Link>
-    <Link to="/contato" className="link-rota">Contato</Link>
-    <Link to="/historico-conquistas" className="link-rota">Historico  conquistas</Link>
-    <Link to="/notificacoes" className="link-rota">Notificacoes</Link>
-    <Link to="/chat" className="link-rota">Chat</Link>
-    <Link to="/upload-imagem" className="link-rota">Imagem</Link>
-    <Link to="/avaliacao-partida" className="link-rota">Avaliacao partida</Link>
-    <Link to="/convidar-amigos" className="link-rota">Convidar amigos</Link>
+      <div className="rotas">
+        <Link to="/login" className="link-rota">Login</Link>
+        <Link to="/cadastro" className="link-rota">Cadastro</Link>
+        <Link to="/editar-perfil" className="link-rota">Editar Perfil</Link>
+        <Link to="/mapa" className="link-rota">Mapa</Link>
+        <Link to="/favoritar-partida" className="link-rota">Partidas Favoritas</Link>
+        <Link to="/detalhes-partida" className="link-rota">Detalhes Partidas</Link>
+        <Link to="/cadastro-partida" className="link-rota">Cadastro Partidas</Link>
+        <Link to="/historico-partidas" className="link-rota">Historico Partidas</Link>
+        <Link to="/pagamento" className="link-rota">Pagamento</Link>
+        <Link to="/contato" className="link-rota">Contato</Link>
+        <Link to="/historico-conquistas" className="link-rota">Historico conquistas</Link>
+        <Link to="/notificacoes" className="link-rota">Notificacoes</Link>
+        <Link to="/chat" className="link-rota">Chat</Link>
+        <Link to="/upload-imagem" className="link-rota">Imagem</Link>
+        <Link to="/avaliacao-partida" className="link-rota">Avaliacao partida</Link>
+        <Link to="/convidar-amigos" className="link-rota">Convidar amigos</Link>
       </div>     
-    <div className="login-container">
-      <img src="src/images/logo.png" alt="" width="60%" />
-      <span></span>
-      <h1>Faça login na sua conta</h1>
-      <form>
-        <label>Email:</label>
-        <input type="email" value={email} onChange={handleEmailChange} placeholder="Digite seu email" />
-        <br />
-        <label>Senha:</label>
-        <input type="password" value={senha} onChange={handleSenhaChange} placeholder="Digite sua senha" />
-        <br />
-        <GoogleLogin
-          onSuccess={credentialResponse => {
-            console.log(credentialResponse);
-          }}
-          onError={() => {
-            console.log('Login Failed');
-          }}
-        />
-        <label>Lembre-me:</label>
-        <input type="checkbox" checked={lembrar} onChange={handleLembrarChange} />
-        <br />
-        <button onClick={handleEntrarClick}>Entrar</button>
-      </form>
-      <p>Esqueceu a senha? <Link to="/recuperar-senha">Clique aqui</Link></p>
-      <p>Não tem uma conta? <Link to="/cadastro">Cadastre-se aqui</Link></p>
-    </div>
-  </div>  
+      <div className="login-container">
+        <img src="src/images/logo.png" alt="" width="60%" />
+        <h1>Faça login na sua conta</h1>
+        <form>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={handleEmailChange} placeholder="Digite seu email" />
+          <br />
+          <label>Senha:</label>
+          <input type="password" value={senha} onChange={handleSenhaChange} placeholder="Digite sua senha" />
+          <br />
+          <GoogleLogin
+            onSuccess={credentialResponse => {
+              console.log(credentialResponse);
+            }}
+            onError={() => {
+              console.log('Login Failed');
+            }}
+          />
+          <label>Lembre-me:</label>
+          <input type="checkbox" checked={lembrar} onChange={handleLembrarChange} />
+          <br />
+          <button onClick={handleEntrarClick}>Entrar</button>
+        </form>
+        <p>Esqueceu a senha? <Link to="/recuperar-senha">Clique aqui</Link></p>
+        <p>Não tem uma conta? <Link to="/cadastro">Cadastre-se aqui</Link></p>
+      </div>
+    </div>  
   );
 }
 
