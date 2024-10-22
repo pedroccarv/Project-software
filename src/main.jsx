5408ddd41b3026599879d3d990de850e4e00b73c
pedroccarv <ppedrinhohenrique06@hotmail.com>
2024-10-22 17:42:12 -0300

    Update main.jsx
    Colocando a rota privada

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import AppRoutes from './route/route';
 import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { AuthProvider } from './services/AuthContext'; // Adicione isso
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="266963174901-1ro8upe7tgfc77pbba8pktf4qoorajit.apps.googleusercontent.com">
-      <AppRoutes />
+    <AuthProvider> {/* Adicione o AuthProvider */}
+        <AppRoutes />
+      </AuthProvider>
     </GoogleOAuthProvider>
   </React.StrictMode>,
 );
