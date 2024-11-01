713685da6a43efd5d092861f2f114e094531329c
pedroccarv <ppedrinhohenrique06@hotmail.com>
2024-11-01 18:40:34 -0300

    Update route.jsx

--- src/route/route.jsx
+++ src/route/route.jsx
@@ -1,24 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from '../components/Login';
 import Cadastro from '../components/Cadastro';
-import RecuperarSenha from '../components/RecuperarSenha';
 import EditarPerfil from '../components/EditarPerfil';
-import Mapa from '../components/Mapa';
-import FavoritarPartida from '../components/FavoritarPartida';
 import Partidas from '../models/Partidas';
-import DetalhesPartida from '../pages/DetalhesPartida';
-import CadastroPartida from '../controllers/CadastroPartida';
-import HistoricoPartidas from '../pages/HistoricoPartidas';
 import Pagamento from '../pages/Pagamento';
 import Contato from '../pages/Contato';
-import Chat from '../components/chat';
 import Notificacoes from '../components/Notificacoes';
-import UploadImagem from '../components/UploadImagem';
-import AvaliacaoPartida from '../components/AvaliacaoPartida';
 import ConvidarAmigos from '../components/ConvidarAmigos';
-import HistoricoConquistas from '../components/HistoricoConquistas';
 import Agendar from '../components/Agendarquadra';
-import ContadorPartidas from '../components/ContadorPartidas';
 import Usuario from '../components/Usuario';
 import PrivateRoute from '../route/privateRoute'; // Importe o PrivateRoute
 import Pagina from '../components/LandingPage'
@@ -28,13 +17,11 @@ function AppRoutes() {
     <BrowserRouter>
       <Routes>
         {/* Página inicial de Cadastro */}
-        <Route path="/" element={<Cadastro />} />
+        <Route path="/" element={<Pagina />} />
 
         {/* Rotas de autenticação */}
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/recuperar-senha" element={<RecuperarSenha />} />
-
         {/* Rotas de perfil e ajustes */}
         <Route
           path="/editar-perfil"
@@ -44,25 +31,6 @@ function AppRoutes() {
             </PrivateRoute>
           }
         />
-
-        {/* Mapa e funcionalidades de arena */}
-        <Route
-          path="/mapa"
-          element={
-            <PrivateRoute>
-              <Mapa />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/favoritar-partida"
-          element={
-            <PrivateRoute>
-              <FavoritarPartida />
-            </PrivateRoute>
-          }
-        />
-
         {/* Rotas relacionadas a partidas */}
         <Route
           path="/partidas"
@@ -72,39 +40,6 @@ function AppRoutes() {
             </PrivateRoute>
           }
         />
-        <Route
-          path="/detalhes-partida/:id"
-          element={
-            <PrivateRoute>
-              <DetalhesPartida />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/cadastro-partida"
-          element={
-            <PrivateRoute>
-              <CadastroPartida />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/historico-partidas"
-          element={
-            <PrivateRoute>
-              <HistoricoPartidas />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/contador-partidas"
-          element={
-            <PrivateRoute>
-              <ContadorPartidas />
-            </PrivateRoute>
-          }
-        />
-
         {/* Funcionalidades adicionais */}
         <Route
           path="/pagamento"
@@ -123,34 +58,10 @@ function AppRoutes() {
           }
         />
         <Route
-          path="/chat"
-          element={
-            <PrivateRoute>
-              <Chat />
-            </PrivateRoute>
-          }
-        />
-        <Route
           path="/notificacoes"
           element={
             <PrivateRoute>
               <Notificacoes />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/upload-imagem"
-          element={
-            <PrivateRoute>
-              <UploadImagem />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/avaliacao-partida"
-          element={
-            <PrivateRoute>
-              <AvaliacaoPartida />
             </PrivateRoute>
           }
         />
@@ -162,15 +73,6 @@ function AppRoutes() {
             </PrivateRoute>
           }
         />
-        <Route
-          path="/historico-conquistas"
-          element={
-            <PrivateRoute>
-              <HistoricoConquistas />
-            </PrivateRoute>
-          }
-        />
-
         {/* Agendamento de quadra */}
         <Route
           path="/agendar"
