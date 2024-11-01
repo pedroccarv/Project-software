01bf2539d9ae4c9aaff46af811cc5a27f01a478d
pedroccarv <ppedrinhohenrique06@hotmail.com>
2024-11-01 18:40:20 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import AppRoutes from './route/route';
 import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider } from './services/AuthContext'; // Adicione isso
+import { Toaster } from 'sonner'; // Importando o Toaster
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="266963174901-1ro8upe7tgfc77pbba8pktf4qoorajit.apps.googleusercontent.com">
-    <AuthProvider> {/* Adicione o AuthProvider */}
+      <AuthProvider> {/* Adicione o AuthProvider */}
+        <Toaster /> {/* Adicione o Toaster aqui */}
         <AppRoutes />
       </AuthProvider>
     </GoogleOAuthProvider>
