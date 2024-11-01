e3e22864047cdf4257661d2328ba5cfb693ab202
pedroccarv <ppedrinhohenrique06@hotmail.com>
2024-11-01 18:39:51 -0300

    Update LandingPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,9 +3,6 @@ import { useAuth } from '../services/AuthContext'; // Importando o contexto de a
 import '../styles/LandingPage.css'; // Importe um CSS para estilos
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons';
 import NavBar from './NavBar';
 
 const LandingPage = () => {
@@ -13,8 +10,8 @@ const LandingPage = () => {
   const [posts, setPosts] = useState([]);
   const [postContent, setPostContent] = useState('');
   const [postImage, setPostImage] = useState(null);
-  const [showDropdown, setShowDropdown] = useState(false); // Estado para controlar o menu suspenso
   const navigate = useNavigate(); // Chama o useNavigate aqui
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -42,11 +39,9 @@ const LandingPage = () => {
 
     try {
       const response = await axios.post('http://localhost:3000/posts', newPost);
-
-      // Cria um novo post incluindo os dados do usuário
       const postWithUser = {
         ...response.data,
-        user: { id: response.data.user.id, name: response.data.user.name }, // Inclua o nome do usuário
+        user: { id: response.data.user.id, name: response.data.user.name },
       };
 
       setPosts([postWithUser, ...posts]); // Adiciona a nova postagem no início
@@ -80,85 +75,67 @@ const LandingPage = () => {
     }
   };
 
-  // Função para alternar o menu suspenso
-  const toggleDropdown = () => {
-    setShowDropdown(!showDropdown);
-  };
-
   return (
     <>
-    <NavBar />
-    <div className="landing-page">
-      <header className="header">
-        <h1>Meu Feed</h1>
+      <NavBar />
+      <div className="landing-page">
+        <header className="header">
+          <h1>Meu Feed</h1>
+        </header>
 
-        {/* Menu de usuário no canto superior direito */}
-        {user && (
-          <Menu as="div" className="relative inline-block text-left">
-            <div>
-
-              <MenuButton className="flex items-center w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                <span className='flex justify-center items-center'> <img
-                  alt=""
-                  FontAwesomeIcon icon="fa-solid fa-user" 
-                  className="inline-block h-6 w-6 rounded-full ring-2 ring-white"
-                /></span>
-                {user?.name || 'Usuário'}
-                <svg width="20px" height="20px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 14a.997.997 0 01-.707-.293l-5-5a.999.999 0 111.414-1.414L10 11.586l4.293-4.293a.999.999 0 111.414 1.414l-5 5A.997.997 0 0110 14z" fill="#5C5F62" /></svg>
-              </MenuButton>
-            </div>
-
-            <MenuItems
-              transition
-              className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-            >
-              <div className="py-1">
-                <MenuItem>
-                  <button onClick={() => navigate('/editar-perfil')}>Editar Perfil</button>
-                </MenuItem>
-                <MenuItem>
-                  <button onClick={logout}>Sair</button>
-                </MenuItem>
-
-              </div>
-            </MenuItems>
-          </Menu>
+        {user ? (
+          <div className="post-form">
+<form onSubmit={handlePostSubmit}>
+  <label htmlFor="postContent" className="sr-only">Sua mensagem</label>
+  <div className="flex items-center px-3 py-2 rounded-lg bg-gray-50 dark:bg-gray-700">
+    <label htmlFor="imageUpload" className="cursor-pointer inline-flex justify-center p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+      <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
+        <path fill="currentColor" d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z"/>
+        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 1H2a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"/>
+      </svg>
+      <span className="sr-only">Upload image</span>
+    </label>
+    <input
+      id="imageUpload"
+      type="file"
+      accept="image/*"
+      onChange={(e) => setPostImage(URL.createObjectURL(e.target.files[0]))}
+      className="hidden" // Continue a usar hidden, mas envolva com o label
+    />
+    <textarea
+      id="postContent"
+      rows="1"
+      className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      placeholder="O que você está pensando?"
+      value={postContent}
+      onChange={(e) => setPostContent(e.target.value)}
+      required
+    />
+    <button type="submit" className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
+      <svg className="w-5 h-5 rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+        <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z"/>
+      </svg>
+      <span className="sr-only">Enviar mensagem</span>
+    </button>
+  </div>
+</form>
+          </div>
+        ) : (
+          <p>Você precisa estar logado para fazer uma postagem.</p>
         )}
-      </header>
 
-      {user ? (
-        <div className="post-form">
-          <form onSubmit={handlePostSubmit}>
-            <textarea
-              placeholder="O que você está pensando?"
-              value={postContent}
-              onChange={(e) => setPostContent(e.target.value)}
-              required
+        <div className="posts-feed">
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              post={post}
+              user={user}
+              onDeletePost={handleDeletePost}
+              onEditPost={handleEditPost}
             />
-            <input
-              type="file"
-              accept="image/*"
-              onChange={(e) => setPostImage(URL.createObjectURL(e.target.files[0]))}
-            />
-            <button type="submit">Postar</button>
-          </form>
+          ))}
         </div>
-      ) : (
-        <p>Você precisa estar logado para fazer uma postagem.</p>
-      )}
-
-      <div className="posts-feed">
-        {posts.map((post) => (
-          <Post
-            key={post.id}
-            post={post}
-            user={user}
-            onDeletePost={handleDeletePost}
-            onEditPost={handleEditPost}
-          />
-        ))}
       </div>
-    </div>
     </>
   );
 };
@@ -182,18 +159,13 @@ const Post = ({ post, user, onDeletePost, onEditPost }) => {
     setIsEditing(false);
   };
 
-  // Verifique se post.user está definido
   const userName = post.user?.name || 'Usuário Desconhecido';
 
   return (
-    <>
     <div className="post">
       <p>
         <strong>{userName}</strong> - {new Date(post.createdAt).toLocaleString()}
       </p>
-
-      
-
       {isEditing ? (
         <div>
           <textarea
@@ -206,10 +178,7 @@ const Post = ({ post, user, onDeletePost, onEditPost }) => {
       ) : (
         <p>{post.content}</p>
       )}
-
       {post.image && <img src={post.image} alt="Post" width="200" />}
-
-      {/* Botões de Editar e Excluir apenas se o usuário for o autor do post */}
       {user && user.id === post.userId && (
         <div className="post-actions">
           <button onClick={handleEditClick}>Editar</button>
@@ -217,7 +186,6 @@ const Post = ({ post, user, onDeletePost, onEditPost }) => {
         </div>
       )}
     </div>
-    </>
   );
 };
 
