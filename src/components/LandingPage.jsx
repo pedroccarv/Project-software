8de0a8f3cbff9203bd8c7f13ef5d5992d4234ec0
GABRIEL_UTSCH_PIMENTA_CARVALHO <12200271@OMEGAB49.BARROCA>
2024-11-05 08:09:01 -0300

    style

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -79,52 +79,54 @@ const LandingPage = () => {
     <>
       <NavBar />
       <div className="landing-page">
-        <header className="header">
-          <h1>Meu Feed</h1>
-        </header>
+        
+        <div className='w-full'>
+          <header className="header">
+            <h1>Meu Feed</h1>
+          </header>
+          {user ? (
+            <div className="post-form">
+              <form onSubmit={handlePostSubmit}>
+                <label htmlFor="postContent" className="sr-only">Sua mensagem</label>
+                <div className="flex items-center  rounded-lg bg-gray-50 dark:bg-gray-700">
+                  <label htmlFor="imageUpload" className="cursor-pointer inline-flex justify-center p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
+                      <path fill="currentColor" d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z" />
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 1H2a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
+                    </svg>
+                    <span className="sr-only">Upload image</span>
+                  </label>
+                  <input
+                    id="imageUpload"
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => setPostImage(URL.createObjectURL(e.target.files[0]))}
+                    className="hidden" // Continue a usar hidden, mas envolva com o label
+                  />
+                  <input
+                    id="postContent"
+                    rows="1"
+                    className="block resize-none mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="O que você está pensando?"
+                    value={postContent}
+                    onChange={(e) => setPostContent(e.target.value)}
+                    required
+                  />
+                  <button type="submit" className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
+                    <svg className="w-5 h-5 rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                      <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
+                    </svg>
+                    <span className="sr-only">Enviar mensagem</span>
+                  </button>
+                </div>
+              </form>
+            </div>
+          ) : (
+            <p>Você precisa estar logado para fazer uma postagem.</p>
+          )}
+        </div>
 
-        {user ? (
-          <div className="post-form">
-<form onSubmit={handlePostSubmit}>
-  <label htmlFor="postContent" className="sr-only">Sua mensagem</label>
-  <div className="flex items-center px-3 py-2 rounded-lg bg-gray-50 dark:bg-gray-700">
-    <label htmlFor="imageUpload" className="cursor-pointer inline-flex justify-center p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
-      <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
-        <path fill="currentColor" d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z"/>
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 1H2a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"/>
-      </svg>
-      <span className="sr-only">Upload image</span>
-    </label>
-    <input
-      id="imageUpload"
-      type="file"
-      accept="image/*"
-      onChange={(e) => setPostImage(URL.createObjectURL(e.target.files[0]))}
-      className="hidden" // Continue a usar hidden, mas envolva com o label
-    />
-    <textarea
-      id="postContent"
-      rows="1"
-      className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      placeholder="O que você está pensando?"
-      value={postContent}
-      onChange={(e) => setPostContent(e.target.value)}
-      required
-    />
-    <button type="submit" className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
-      <svg className="w-5 h-5 rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-        <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z"/>
-      </svg>
-      <span className="sr-only">Enviar mensagem</span>
-    </button>
-  </div>
-</form>
-          </div>
-        ) : (
-          <p>Você precisa estar logado para fazer uma postagem.</p>
-        )}
-
-        <div className="posts-feed">
+        <div className="posts-feed w-full">
           {posts.map((post) => (
             <Post
               key={post.id}
